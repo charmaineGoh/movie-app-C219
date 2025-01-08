1051dd8bd7a810736c64d4c659c771896f1af09b
charmaineGoh <23021210@myrp.edu.sg>
2025-01-08 16:07:30 +0800

    update

---
+++ movie-app-starter-main/src/App.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+const KEY = "951dc136";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [query,setQuery]=useState("batman");
+
+
+
+  useEffect(() => {
+    const controller = new AbortController();
+    // Effect
+    fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,{signal:controller.signal})
+      .then((res) => res.json())
+      .then((data) => data.Response=="True" &&setMovies(data.Search))
+      .catch((err) => console.log(err));
+      return () => {controller.abort();
+       };
+
+  }, [query]);
+
+  return (
+    <div>
+      <h1>Movies</h1>
+      <input
+        value={query}
+        type="text"
+        placeholder="Search movies..."
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.imdbID}>
+              <td>{movie.Title}</td>
+              <td>{movie.Year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
